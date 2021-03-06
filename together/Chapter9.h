4d4b0cee04100e463063ba6d7d4103422691b5ba
Rick James <rkj@fb.com>
2021-03-06 15:25:20 -0800

    chapter 9b together demo

--- together/Chapter9.h
+++ together/Chapter9.h
@@ -18,32 +18,23 @@ class Chapter9
 {
 
 public:
+	void addhex(std::vector<std::unique_ptr<Shape>>& vec) {
+		for (int i = 0; i < 6; i++) {
+			auto wall = std::make_unique<Plane>();
+			wall->transform = Matrix4::rotateY(i * 2 * pi / 6) * Matrix4::translate(0, 0, 12) * Matrix4::rotateX(pi / 2);
+			wall->material.color = Color(.5, 0.9f, 0.9f);
+			wall->material.specular = 0;
+			vec.push_back(std::move(wall));
+		}
+	}
+
 	void run() {
 		std::unique_ptr<Plane> floor = std::make_unique<Plane>();
-		// floor.transform = Matrix4::scale(10, 0.01f, 10);
 		floor->material.color = Color(1, 0.9f, 0.9f);
 		floor->material.specular = 0;
 
-		/*
-		Sphere left_wall;
-		left_wall.transform =
-			Matrix4::translate(0,0,5)*
-			Matrix4::rotateY(-pi/4) *
-			Matrix4::rotateX(pi/2) *
-			Matrix4::scale(10, 0.01f, 10);
-		left_wall.material = floor.material;
-
-		Sphere right_wall;
-		right_wall.transform =
-			Matrix4::translate(0, 0, 5) *
-			Matrix4::rotateY(pi / 4) *
-			Matrix4::rotateX(pi / 2) *
-			Matrix4::scale(10, 0.01f, 10);
-		right_wall.material = floor.material;
-		*/
-
 		std::unique_ptr<Sphere> middle = std::make_unique<Sphere>();
-		middle->transform = Matrix4::translate(-0.5, 1, 0.5f);
+		middle->transform = Matrix4::translate(-0.5, .5, 0.5f);
 		middle->material.color = Color(0.1f, 1, 0.5f);
 		middle->material.diffuse = 0.7f;
 		middle->material.specular = 0.3f;
@@ -66,19 +57,21 @@ public:
 		vec.push_back(std::move(right));
 		vec.push_back(std::move(left));
 
-		PointLight light(Point3(-10, 10, -10), Color(1, 1, 1));
+		addhex(vec);
+
+		PointLight light(Point3(-7, 7, -7), Color(1, 1, 1));
 
 		World world(light, std::move(vec));
 
-		Camera camera(2000, 1000, pi / 3);
-		Point3 from(0, 1.5, -5);
+		Camera camera(3840, 2160, pi / 3);
+		Point3 from(0, 8, -1);
 		Point3 to(0, 1, 0);
 		Vec3 up(0, 1, 0);
 		camera.transform = Matrix4::view(from, to, up);
 
 		Canvas canvas = camera.render(world);
 
-		std::ofstream ostrm("chap9.ppm", std::ios::binary);
+		std::ofstream ostrm("chap9b.ppm", std::ios::binary);
 		canvas.write_ppm(ostrm);
 	}
 };
