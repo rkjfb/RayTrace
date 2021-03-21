6d270eb53ff73d00090e0d4fe0d3bb7aafde779c
Rick James <rkj@fb.com>
2021-03-20 22:03:04 -0700

    Multithread render

--- lib/Camera.cpp
+++ lib/Camera.cpp
@@ -1,2 +1,78 @@
 #include "pch.h"
 #include "Camera.h"
+
+using namespace ray;
+
+Camera::Camera(int h, int v, double field) :
+	hsize(h), vsize(v), fov(field) {
+
+	double half_view = tan(field / 2.0f);
+	double aspect = (double)h / v;
+
+	if (aspect >= 1) {
+		half_width = half_view;
+		half_height = half_view / aspect;
+	}
+	else {
+		half_width = half_view * aspect;
+		half_height = half_view;
+	}
+
+	pixel_size = half_width * 2 / hsize;
+}
+
+Ray Camera::ray(int px, int py) const {
+	// offset from edge of canvas to center of pixel
+	double x = (px + 0.5f) * pixel_size;
+	double y = (py + 0.5f) * pixel_size;
+
+	// pixel in world space. note that camera is facing -z, so +x is left.
+	double wx = half_width - x;
+	double wy = half_height - y;
+
+	Matrix4 inv = transform.inverse();
+	Point3 pixel = inv * Point3(wx, wy, -1);
+	Point3 origin = inv * Point3(0, 0, 0);
+	Vec3 direction = (pixel - origin).norm();
+
+	return Ray(origin, direction);
+}
+
+
+void Camera::fill_column(const World* world, std::vector<std::vector<Color>>& pixels) {
+	for (;;) {
+		int localx = x.fetch_add(1);
+		if (localx >= hsize) {
+			return;
+		}
+
+		for (int y = 0; y < vsize; y++) {
+			Ray r = ray(localx, y);
+			Color c = world->color_at(r);
+			pixels[localx][y] = c;
+		}
+	}
+}
+
+Canvas Camera::render(const World& world) {
+	auto start = std::chrono::steady_clock::now();
+	Canvas image(hsize, vsize);
+
+	x.store(0);
+	std::vector<std::thread> threads;
+	for (uint32_t i = 0; i < std::thread::hardware_concurrency(); i++) {
+		threads.emplace_back(std::thread([&] { fill_column(&world, image.pixels()); }));
+	}
+
+	for (auto& t : threads) {
+		t.join();
+	}
+
+	auto end = std::chrono::steady_clock::now();
+	std::chrono::duration<double> durdiff = end - start;
+	double diff = durdiff.count();
+	double megapixels_per_sec = hsize * vsize / diff / 1000000;
+	std::cout << std::setw(9) << hsize << "x" << vsize << " render time: " << diff << "s " << megapixels_per_sec << " megapixels/sec" << std::endl;
+
+	return image;
+}
--- lib/Camera.h
+++ lib/Camera.h
@@ -3,6 +3,7 @@
 #include <chrono>
 #include <iostream>
 #include <iomanip>
+#include <atomic>
 #include "Matrix.h"
 #include "World.h"
 #include "Canvas.h"
@@ -12,69 +13,24 @@ namespace ray {
 class Camera
 {
 public:
-	Camera(int h, int v, double field) :
-		hsize(h), vsize(v), fov(field) {
+	Camera(int h, int v, double field);
+	Ray ray(int px, int py) const;
+	void fill_column(const World* world, std::vector<std::vector<Color>>& pixels);
+	Canvas render(const World& world);
 
-		double half_view = tan(field / 2.0f);
-		double aspect = (double)h / v;
-		
-		if (aspect >= 1) {
-			half_width = half_view;
-			half_height = half_view / aspect;
-		}
-		else {
-			half_width = half_view * aspect;
-			half_height = half_view;
-		}
-
-		pixel_size = half_width * 2 / hsize;
-	}
-
-	Ray ray(int px, int py) const {
-		// offset from edge of canvas to center of pixel
-		double x = (px + 0.5f) * pixel_size;
-		double y = (py + 0.5f) * pixel_size;
-
-		// pixel in world space. note that camera is facing -z, so +x is left.
-		double wx = half_width - x;
-		double wy = half_height - y;
-
-		Matrix4 inv = transform.inverse();
-		Point3 pixel = inv * Point3(wx, wy, -1);
-		Point3 origin = inv * Point3(0, 0, 0);
-		Vec3 direction = (pixel - origin).norm();
-
-		return Ray(origin, direction);
-	}
-
-	Canvas render(const World& world) {
-		auto start = std::chrono::steady_clock::now();
-		Canvas image(hsize, vsize);
-		auto& pixels = image.pixels();
-
-		for (int y = 0; y < vsize; y++) {
-			for (int x = 0; x < hsize; x++) {
-				Ray r = ray(x, y);
-				Color c = world.color_at(r);
-				pixels[x][y] = c;
-			}
-		}
-		auto end = std::chrono::steady_clock::now();
-		std::chrono::duration<double> durdiff = end - start;
-		double diff = durdiff.count();
-		double megapixels_per_sec = hsize * vsize / diff / 1000000;
-		std::cout << std::setw(9) << hsize << "x" << vsize <<" render time: " << diff << "s " << megapixels_per_sec << " megapixels/sec" << std::endl;
-
-		return image;
-	}
-
+public:
 	int hsize;
-	double half_width;
 	int vsize;
-	double half_height;
 	double fov;
-	Matrix4 transform;
 	double pixel_size;
+	Matrix4 transform;
+
+private:
+	// shared thread state
+	std::atomic_int x;
+
+	double half_width;
+	double half_height;
 };
 
 } // namespace ray
