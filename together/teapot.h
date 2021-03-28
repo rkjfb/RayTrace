b6ba66d278339e0ed3402919e254502fc44f3dd9
Rick James <rkj@fb.com>
2021-03-28 13:02:04 -0700

    fix teapot render camera

--- together/teapot.h
+++ together/teapot.h
@@ -28,12 +28,21 @@ public:
 		std::vector<std::unique_ptr<Shape>> vec;
 		vec.push_back(w.get_mega_group());
 
+		{
+			auto s = std::make_unique<Cube>();
+			double scale = 20;
+			s->transform = Matrix4::scale(scale, scale, scale);
+			s->material.pattern = std::make_unique<Checker>(Color::darkorange(), Color::yellow());
+			s->material.pattern->transform = Matrix4::scale(1 / scale, 1 / scale, 1 / scale);
+			vec.push_back(std::move(s));
+		}
+
 		PointLight light(Point3(-10, 10, -10), Color(1, 1, 1));
 
 		World world(light, std::move(vec));
 
-		Camera camera(200, 100, pi / 3);
-		Point3 from(5, 2, -5);
+		Camera camera(400, 200, pi / 3);
+		Point3 from(0, 10, -10);
 		Point3 to(0, 0, 0);
 		Vec3 up(0, 1, 0);
 		camera.transform = Matrix4::view(from, to, up);
