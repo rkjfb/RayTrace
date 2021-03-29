338863c8737e493c8d479897ef12909d4f5f0a32
Rick James <rkj@fb.com>
2021-03-28 21:58:16 -0700

    demo cows and teapots

--- together/teapot.h
+++ together/teapot.h
@@ -23,29 +23,50 @@ public:
 	void run() {
 		std::vector<std::unique_ptr<Shape>> vec;
 		{
+			std::ifstream file("teapot_normals.obj");
+			Wavefront w(file);
+
+			auto g = w.get_mega_group();
+			Bounds bounds = g->bounds();
+			g->transform = Matrix4::translate(-4, 0, 0) * Matrix4::scale(0.2, 0.2, 0.2) * Matrix4::rotateX(-pi / 2);
+			vec.push_back(std::move(g));
+		}
+
+		{
+			std::ifstream file("teapot.obj");
+			Wavefront w(file);
+
+			auto g = w.get_mega_group();
+			Bounds bounds = g->bounds();
+			g->transform = Matrix4::translate(4, 0, 0) * Matrix4::rotateY(pi);
+			vec.push_back(std::move(g));
+		}
+
+		for (int x = -10; x < 11; x += 5)
+		{
 			std::ifstream file("cow.obj");
 			Wavefront w(file);
 
 			auto g = w.get_mega_group();
-			g->transform = Matrix4::translate(0,-g->bounds().min.y, 0) * Matrix4::rotateY(pi / 2);
+			g->transform = Matrix4::translate(x, -g->bounds().min.y, 10) * Matrix4::rotateY(pi / 2);
 			vec.push_back(std::move(g));
 		}
 
 		{
 			auto s = std::make_unique<Cube>();
-			double scale = 20;
+			double scale = 25;
 			s->transform = Matrix4::translate(0,scale,0) * Matrix4::scale(scale, scale, scale);
 			s->material.pattern = std::make_unique<Checker>(Color::green(), Color::blanchedalmond());
 			s->material.pattern->transform = Matrix4::scale(1 / scale, 1 / scale, 1 / scale);
 			vec.push_back(std::move(s));
 		}
 
-		PointLight light(Point3(-10, 10, -10), Color(1, 1, 1));
+		PointLight light(Point3(-5, 15, -20), Color(1, 1, 1));
 
 		World world(light, std::move(vec));
 
 		Camera camera(4000, 2000, pi / 3);
-		Point3 from(-8, 10, -15 );
+		Point3 from(0, 10, -15);
 		Point3 to(0, 3, 0);
 		Vec3 up(0, 1, 0);
 		camera.transform = Matrix4::view(from, to, up);
